4172b0651cb90eaa08fa45ab40d1fa29f532f933
Isabel003 <mlopezs56@miumg.edu.gt>
2024-01-28 22:31:19 -0600

    Laboratorio 1

---
+++ Laboratorio1/grados.py
@@ -0,0 +1,10 @@
+def celsiusAfah(celsius):
+   fahrenheit = ((celsius* 9/5)+32)
+   return fahrenheit
+
+celsius=float(input("Ingrese grados Celsius: "))
+fahrenheit = celsiusAfah(celsius)
+print (f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit.")
+
+
+
